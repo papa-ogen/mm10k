e644cd3859aaf1c29c06ca048205559850629e4f
Jörgen Thelin <thelin.jorgen@gmail.com>
2018-07-02 16:09:03 +0200

    Weather module added

---
+++ helpers.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+
+// inserting an SVG
+exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set('view engine', 'pug')
 app.use(express.static('public'))
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  res.render('index', { weatherData: {"location":"Skälby","weathers":[{"date":"Måndag","temp":"12.7","type":"clouds","descr":"klar himmel"},{"date":"Tisdag","temp":"10.3","type":"broken-cloud","descr":"blandade moln"},{"date":"Onsdag","temp":"12.5","type":"rain","descr":"blandade moln"}]} })
 })
 
 
