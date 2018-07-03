655072a5927ebf03901c106a9c641248738fd40f
Jörgen Thelin <thelin.jorgen@gmail.com>
2018-07-03 12:41:20 +0200

    Changed to promise

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,65 @@ const router = express.Router();
 const weatherController = require('../controllers/weatherController');
 const request = require('request');
 
-const apiParams = {
-  apiUrl: 'http://api.openweathermap.org/data/2.5/forecast',
-  apiKey: process.env.WEATHER_KEY,
-  coords: {
-    lat: 59.3833,
-    lon: 17.8333
-  }
-}
-const requestUrl = weatherController.createRequestUrl(apiParams)
+// const apiParams = {
+//   apiUrl: 'http://api.openweathermap.org/data/2.5/forecast',
+//   apiKey: process.env.WEATHER_KEY,
+//   coords: {
+//     lat: 59.3833,
+//     lon: 17.8333
+//   }
+// }
+// const requestUrl = weatherController.createRequestUrl(apiParams)
 
-router.get('/', function (req, res) {
-  request(requestUrl, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
+const getData = url => {
+  const options = {
+      url: url,
+      headers: {
+          'User-Agent': 'request'
+      }
+  };
+
+  return new Promise((resolve, reject) => {
+      request.get(options, (err, resp, body) => {
+          if (err) {
+              reject(err);
+          } else {
+              resolve(body);
+          }
+      })
+  })
+}
+
+const errHandler = function(err) {
+    console.log('Error', err);
+}
+
+const main = (req, res) => {
+  const apiParams = {
+    apiUrl: 'http://api.openweathermap.org/data/2.5/forecast',
+    apiKey: process.env.WEATHER_KEY,
+    coords: {
+      lat: 59.3833,
+      lon: 17.8333
+    }
+  }
+  const requestUrl = weatherController.createRequestUrl(apiParams)
+  getData(requestUrl)
+  .then(JSON.parse, errHandler)
+  .then((result) => {
       res.render('index', { 
         title: "MM10K", 
-        weatherData: weatherController.filterData(JSON.parse(response.body)) 
+        weatherData: weatherController.filterData(result)
       })
-    } else {
-      console.log(error, response)
-    }
-    })
+      // Do one more async operation here
+      // var anotherPromise = getData(userDetails.followers_url).then(JSON.parse);
+      // return anotherPromise;
+  }, errHandler)
+  .catch(console.error)
+}
+
+router.get('/', function (req, res) {
+  main(req, res)
 })
 
 router.get('/weather', weatherController.homePage);
