4c73b5ccc221448f3b09e2c7e4b4464b59935207
Jörgen Thelin <thelin.jorgen@gmail.com>
2018-07-04 15:11:00 +0200

    Socket.io

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
 const express = require('express')
-const pug = require('pug');
 const helpers = require('./helpers');
 const routes = require('./routes/index');
 
-const app = express()
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
+app.set('views', __dirname + '/views');
 app.set('view engine', 'pug')
 
 app.use(express.static('public'))
@@ -17,4 +19,17 @@ app.use((req, res, next) => {
 
 app.use('/', routes);
 
-app.listen(3000, () => console.log('App listening on port 3000!'))
+setInterval(function() {
+  io.emit('data', 'random number: ' + Math.random().toString());
+}, 1000);
+
+io.on('connection', function (socket) {
+  console.log('client connected');       
+  socket.on('disconnect', function() {
+      console.log('client disconnected');
+  });  
+});
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
