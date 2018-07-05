e3b65ee1b3a1daf1880f912f98ef7463c4a24d58
Jörgen Thelin <thelin.jorgen@gmail.com>
2018-07-05 11:33:41 +0200

    Moved code

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const routes = require('./src/index');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const ontime = require('ontime');
-const api = require('./src/api');
 const socketSrc = require('./src/socketSrc')
 
 app.set('views', __dirname + '/views');
@@ -23,35 +21,34 @@ app.use((req, res, next) => {
 app.use('/', routes);
 
 // Update clock
-const clock = require('./controllers/clockController')
+socketSrc.updateClock(io);
 
-setInterval(function() {
-  io.emit('clock', clock.getCurrentTime());
-}, 1000);
-
-ontime({
-  cycle: [ '00:00:01']
-}, function (ot) {
-  // Get todays name
-  const requestUrl = 'http://api.dryg.net/dagar/v2.1/'
-  api.getData(requestUrl)
-  .then(JSON.parse, api.errHandler)
-  .then((data) => {
-      io.emit('todaysNames', data.dagar[0].namnsdag.join(', '));
-    }, api.errHandler)
-    .catch(console.error)
-
-  ot.done()
-  return
-})
-
+// Get bg images
 socketSrc.getBgImages(io);
 
-io.on('connection', function (socket) {
-  
+// Get todays name
+socketSrc.getTodaysName(io);
 
+// ontime({
+//   cycle: [ '00:00:01']
+// }, function (ot) {
+//   const requestUrl = 'http://api.dryg.net/dagar/v2.1/'
+//   api.getData(requestUrl)
+//   .then(JSON.parse, api.errHandler)
+//   .then((data) => {
+//       io.emit('todaysNames', data.dagar[0].namnsdag.join(', '));
+//     }, api.errHandler)
+//     .catch(console.error)
+
+//   ot.done()
+//   return
+// })
+
+io.on('connection', function (socket) {
+  console.log('client connected');
+  
   socket.on('disconnect', function() {
-      console.log('client disconnected');
+    console.log('client disconnected');
   });  
 });
 
--- src/socketSrc.js
+++ src/socketSrc.js
@@ -1,4 +1,6 @@
 const fs = require('fs');
+const ontime = require('ontime');
+const api = require('./api');
 
 exports.getBgImages = (io) => {
   const folder = './public/backgrounds/';
@@ -18,5 +20,31 @@ exports.getBgImages = (io) => {
     io.on('connection', function (socket) {
       socket.emit('bg-image', imageFiles);
     });
+  })
+}
+
+exports.updateClock = (io) => {
+  const clock = require('../controllers/clockController');
+
+  setInterval(function() {
+    io.emit('clock', clock.getCurrentTime());
+  }, 1000);
+}
+
+exports.getTodaysName = (io) => {
+  const requestUrl = 'http://api.dryg.net/dagar/v2.1/'
+
+  ontime({
+    cycle: [ '00:01:00']
+  }, function (ot) {
+    api.getData(requestUrl)
+    .then(JSON.parse, api.errHandler)
+    .then((data) => {
+        io.emit('todaysNames', data.dagar[0].namnsdag.join(', '));
+      }, api.errHandler)
+      .catch(console.error)
+  
+    ot.done()
+    return
   })
 }
